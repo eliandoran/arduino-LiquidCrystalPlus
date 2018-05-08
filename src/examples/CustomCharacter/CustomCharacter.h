c82add8f5c7c58fb16c8bae2c3940f349574f848
Elian Doran <elinnum@gmail.com>
2018-05-09 00:23:26 +0300

    Modify custom character example

--- src/examples/CustomCharacter/CustomCharacter.h
+++ src/examples/CustomCharacter/CustomCharacter.h
@@ -1,33 +1,55 @@
 #include <LiquidCrystalPlus.h>
 #include <inttypes.h>
 
-uint8_t starsCharacter[] = {
-    0b01010,
-    0b10101,
-    0b01010,
-    0b10101,
-    0b01010,
-    0b10101,
-    0b01010,
-    0b10101,
+uint8_t starsCharacter[8] = {
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000
+};
+
+uint8_t starsCharacterReverse[8] = {
+    0b00000,
+    0b00000,
+    0b00000,
+    0b00000,
+    0b11111,
+    0b11111,
+    0b11111,
+    0b11111
 };
 
 class CustomCharacterPage: public Page {
     CustomCharacterManager<uint8_t> *charMan;
 
     void show() {
-        display->println("Custom character.");
-        charMan = new CustomCharacterManager<uint8_t>(*display, 8, 5, 8);        
+        charMan = new CustomCharacterManager<uint8_t>(*display, 8, 5, 8);
 
-        display->setCursor(0, 1);
-        charMan->printChar(starsCharacter);
-
-        display->setCursor(1, 1);
-        charMan->printChar(starsCharacter);
+        display->setOffset(0, 0);
     }
 
-    void loop() {
-        
+    void loop() {        
         delay(500);
+
+        static int i = 0;
+        static bool which = false;
+
+        if (i > 16) {
+            i = 0;
+        }
+
+        display->setCursor(i, 0);
+        charMan->printChar(which ? starsCharacter : starsCharacterReverse);       
+
+        display->setCursor(i, 1);
+        charMan->printChar(which ? starsCharacter : starsCharacterReverse);       
+
+        which = !which;
+
+        i++;
     }
 };
