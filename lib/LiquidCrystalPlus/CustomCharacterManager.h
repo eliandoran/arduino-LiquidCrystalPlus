da62a1c6528ebb50d79948b981fbc660f99c11c8
Elian Doran <elinnum@gmail.com>
2018-05-09 00:53:17 +0300

    Improve CustomCharacterManager

--- lib/LiquidCrystalPlus/CustomCharacterManager.h
+++ lib/LiquidCrystalPlus/CustomCharacterManager.h
@@ -14,6 +14,7 @@ private:
     int charWidth, charHeight;
     Display &display;
     TRow **charStore;
+    int lastIndex;
 
     short findCharIndex(TRow* rows) {
         Serial.print("Finding character @ ");
@@ -32,34 +33,66 @@ private:
 
     short loadChar(TRow* rows) {
         Serial.println("Not found, loading.");
-        int i = 0;
-        display.lcd->createChar(i, rows);
-        charStore[i] = rows;
+        int charIndex;
+
+        if (lastIndex < maxCharacters) {
+            charIndex = lastIndex++;
+        } else {
+            charIndex = lastIndex;
+        }
+
+        display.lcd->createChar(charIndex, rows);
+        charStore[charIndex] = rows;
+        
+        return charIndex;
     }
 
-public:
+public:    
     CustomCharacterManager(Display &display, int maxCharacters, int charWidth, int charHeight): display(display) {
         this->maxCharacters = maxCharacters;
         this->charWidth = charWidth;
         this->charHeight = charHeight;
         this->charStore = (TRow**)calloc(maxCharacters, sizeof(TRow*));
+        this->lastIndex = 0;
+
+        uint8_t emptyChar[8] = {
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000,
+            0b00000
+        };
+
+        for (int i=0; i<maxCharacters; i++) {
+            display.lcd->createChar(i, emptyChar);
+        }
+
+        delay(500);
+
+        lastIndex = 0;
     }
 
     void printChar(TRow* rows) {        
         int charIndex = findCharIndex(rows);
 
         if (charIndex == CHAR_NOT_FOUND) {
-            uint8_t currentX = display.getCurrentX();
-            uint8_t currentY = display.getCurrentY();
             charIndex = loadChar(rows);
 
-            for (int i=0; i<2; i++) {
-                display.setCursor(currentX, currentY);
-                display.write(charIndex);  
-            }      
-        }
+            /*
+             * HACK: Interestingly enough, it seems that without repositioning the cursor the first
+             * time the custom character is displayed, the character would not display at all.
+             */
+            uint8_t currentX = display.getCurrentX();
+            uint8_t currentY = display.getCurrentY();
+            display.setCursor(currentX, currentY);
 
-        display.write(charIndex);
+            display.write((uint8_t)charIndex);
+        } else {
+            display.write(charIndex);
+        }
     }
 };
 
